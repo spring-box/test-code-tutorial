62533ed4aaa17480b47198a89c59a1d15a08719c
choi-ys <project.log.062@gmail.com>
2024-11-08 22:19:52 +0900

    init: initial commit

---
+++ src/test/java/io/springbox/testcodetutorial/TestCodeTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package io.springbox.testcodetutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestCodeTutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
