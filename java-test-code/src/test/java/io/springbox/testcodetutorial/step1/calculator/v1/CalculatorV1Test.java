5d106ba5fb519862d349f7b078299bfef1759db4
choi-ys <project.log.062@gmail.com>
2024-11-20 00:19:18 +0900

    refactor: @ParameterizedTest와 @MethodSource를 이용한 경우의 수 테스트 통합
    - 완료된 TODO 갱신

--- java-test-code/src/test/java/io/springbox/testcodetutorial/step1/calculator/v1/CalculatorV1Test.java
+++ java-test-code/src/test/java/io/springbox/testcodetutorial/step1/calculator/v1/CalculatorV1Test.java
@@ -2,66 +2,41 @@ package io.springbox.testcodetutorial.step1.calculator.v1;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 @DisplayName("V1:사칙 연산 계산기")
 class CalculatorV1Test {
 
-    private final int firstOperand = 6;
-    private final int secondOperand = 3;
+    private static Stream<Arguments> calculate() {
+        final int firstOperand = 6;
+        final int secondOperand = 3;
 
-    @Test
-    @DisplayName("덧셈 연산")
-    void addition() {
-        // Given
-        final String operator = "+";
-        final int expected = firstOperand + secondOperand;
+        return Stream.of(
+            Arguments.of(firstOperand, "+", secondOperand, firstOperand + secondOperand),
+            Arguments.of(firstOperand, "-", secondOperand, firstOperand - secondOperand),
+            Arguments.of(firstOperand, "*", secondOperand, firstOperand * secondOperand),
+            Arguments.of(firstOperand, "/", secondOperand, firstOperand / secondOperand)
+        );
+    }
 
+    @ParameterizedTest(name = "[{index}] : {0}{1}{2}={3}")
+    @MethodSource
+    @DisplayName("사칙 연산")
+    void calculate(
+        int firstOperand,
+        String operator,
+        int secondOperand,
+        int expected
+    ) {
         // When
         int actual = CalculatorV1.calculate(firstOperand, operator, secondOperand);
 
         // Then
         assertThat(actual).isEqualTo(expected);
-    }
-
-    @Test
-    @DisplayName("뺄셈 연산")
-    void minus() {
-        // Given
-        final String operator = "-";
-
-        // When
-        final int actual = CalculatorV1.calculate(firstOperand, operator, secondOperand);
-
-        // Then
-        assertThat(actual).isEqualTo(firstOperand - secondOperand);
-    }
-
-    @Test
-    @DisplayName("곱셈 연산")
-    void multiply() {
-        // Given
-        final String operator = "*";
-
-        // When
-        final int actual = CalculatorV1.calculate(firstOperand, operator, secondOperand);
-
-        // Then
-        assertThat(actual).isEqualTo(firstOperand * secondOperand);
-    }
-
-    @Test
-    @DisplayName("나눗셈 연산")
-    void division() {
-        // Given
-        final String operator = "/";
-
-        // When
-        final int actual = CalculatorV1.calculate(firstOperand, operator, secondOperand);
-
-        // Then
-        assertThat(actual).isEqualTo(firstOperand / secondOperand);
     }
 
 }
