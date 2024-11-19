bec0e689a048ca0b220753c8a98cf543f60aa0c6
choi-ys <project.log.062@gmail.com>
2024-11-19 21:23:18 +0900

    build: java 기반의 TDD 예제 작성을 위한 module 생성
    - JUnit5 사용을 위한 의존성 추가

---
+++ java-test-code/build.gradle.kts
@@ -0,0 +1,5 @@
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.assertj:assertj-core:3.26.3")
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,2 @@
 rootProject.name = "test-code-tutorial"
+include("java-test-code")
